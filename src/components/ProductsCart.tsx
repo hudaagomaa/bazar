9eb633812cb5c3d4fac09e889acbb0f090cdd12f
hudaagomaa <hudaa.gomaa.dev@gmail.com>
2025-02-24 16:09:44 +0200

    footer

---
+++ src/components/ProductsCart.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function ProductsCart() {
+  return (
+    <div>
+      ProductsCart ProductsCart ProductsCart
+    </div>
+  );
+}
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,12 @@
 import Banner from '@/components/Banner';
+import Products from '@/components/Products';
 
 
 export default function Home() {
   return (
     <div>
       <Banner/>
+      <Products/>
     </div>
   );
 }
